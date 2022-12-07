968d2ac60a56cbc7b17c713d3592e0810149e614
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2022-12-07 13:30:08 +0100

    refactor: use variables to define search space

--- src/model_building.py
+++ src/model_building.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
 import keras_tuner
 
-def bulid_model(hyperparameters:keras_tuner.HyperParameters):
+def build_model(hyperparameters:keras_tuner.HyperParameters):
+    """
+    This function returns a keras model based on hyperparameters choosen by a keras tuner. The function is only meant to be passed to a tuner.
+
+    Args:
+        hyperparameters (keras_tuner.HyperParameters):
+
+    Returns:
+        model (keras.Sequential)
+    """
 
     # define search space
     convolution_layers_total_min = 2
@@ -30,17 +39,17 @@ def bulid_model(hyperparameters:keras_tuner.HyperParameters):
     # add convolutional layers
     # ------------------------
     # first layer
-    filter_total = hyperparameters.Int("filter_total_0", 8, 128, 8)
-    kernel_size = tuple([hyperparameters.Int("kernel_size",2,5,1) for _ in range(2)])
+    filter_total = hyperparameters.Int("filter_total_0", filter_count_min, filter_count_max, filter_count_step)
+    kernel_size = tuple([hyperparameters.Int("kernel_size",kernel_size_min,kernel_size_max,kernel_size_step) for _ in range(2)])
     model.add(tf.keras.layers.Conv2D(filter_total, kernel_size, activation='relu', input_shape=(150, 150, 1)))
     model.add(tf.keras.layers.MaxPooling2D((2,2)))
 
     # layer 2 to n
     # TODO Variable beinhaltet nicht die erste Layer
-    convolution_layers_total = hyperparameters.Int("convolution_layers_total", 2, 10, 1)
+    convolution_layers_total = hyperparameters.Int("convolution_layers_total", convolution_layers_total_min, convolution_layers_total_max, convolution_layers_total_step)
     for layer_index  in range(1, convolution_layers_total+1):
-        filter_total = hyperparameters.Int(f"filter_total_{layer_index}",8, 128, 8)
-        kernel_size = tuple([hyperparameters.Int(f"kernel_size_{layer_index}",2,5,1) for _ in range(2)])
+        filter_total = hyperparameters.Int(f"filter_total_{layer_index}",filter_count_min, filter_count_max, filter_count_step)
+        kernel_size = tuple([hyperparameters.Int(f"kernel_size_{layer_index}", kernel_size_min,kernel_size_max,kernel_size_step) for _ in range(2)])
         model.add(tf.keras.layers.Conv2D(filter_total, kernel_size, activation="relu"))
         model.add(tf.keras.layers.MaxPooling2D((2,2)))
 
@@ -49,10 +58,10 @@ def bulid_model(hyperparameters:keras_tuner.HyperParameters):
     # -------------------------------
     # add dense layers with dropout
     # -------------------------------
-    dense_layers_total = hyperparameters.Int("dense_layers_total", 1, 10, 1)
+    dense_layers_total = hyperparameters.Int("dense_layers_total", dense_layers_total_min, dense_layers_total_max, dense_layers_total_step)
 
     for layer_index in range(dense_layers_total):
-        units = hyperparameters.Int(f"units_{layer_index}", 8, 512, 8)
+        units = hyperparameters.Int(f"units_{layer_index}", units_count_min, units_count_max, units_count_step)
         model.add(tf.keras.layers.Dense(units, activation="relu"))
         if hp.Boolean(f"dropout_{layer_index}"):
             model.add(tf.keras.layers.Dropout(rate=0.25))
